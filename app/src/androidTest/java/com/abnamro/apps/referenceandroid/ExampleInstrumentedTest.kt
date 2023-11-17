bc692e1da32209e6655bc66896181917f784dfa7
Grygorii Gerashchenko <ggerashchenko@gmail.com>
2023-11-17 12:29:11 +0100

    - Added test for a floating button

--- app/src/androidTest/java/com/abnamro/apps/referenceandroid/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/abnamro/apps/referenceandroid/ExampleInstrumentedTest.kt
@@ -1,12 +1,15 @@
 package com.abnamro.apps.referenceandroid
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
 
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,8 +19,14 @@ import org.junit.Assert.*
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
 
-    @Test
-    fun useAppContext() {
+    @get:Rule
+    val activityRule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
 
+    @Test
+    fun testEnvelopFloatingButtonClick() {
+        Espresso.onView(ViewMatchers.withId(R.id.fab))
+            .perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withText("Replace with your own action"))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 }
